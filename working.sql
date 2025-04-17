aa5eba9772b18e5bbfc290eff33143bc5f453ce9
Adaugo Akaluso <22040592+Adaakal@users.noreply.github.com>
2025-04-17 13:00:16 +0000

    task description

--- working.sql
+++ working.sql
@@ -1,3 +1,13 @@
+/**
+TASK: Create a list of participants in Colorado 
+(state code CO) with the columns TEAM, SHIRT_OR_HAT, 
+FIRST_NAME, AND LAST_NAME. Organize the list so that 
+the participants are sorted by their team name, then 
+by whether they want a shirt_or_hat, and finally by 
+their last name. Use descending order for the last
+names and ascending order for the other sorting fields.
+**/
+
 SELECT team, shirt_or_hat, first_name, last_name
 FROM people
 WHERE state_code = 'CO'
